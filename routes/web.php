9ea2488f01eff418ae0f2b0915fc64c90b04d43b
gayiia <gayindus5@gmail.com>
2026-01-04 10:24:54 +0530

    routes update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ShopifyWebhookController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('webhooks/shopify/customers', [ShopifyWebhookController::class, 'handleCustomers'])
+    ->name('webhooks.shopify.customers');
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -10,6 +15,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('customers', [CustomerController::class, 'index'])->name('customers');
+    Route::get('customers/{customer}', [CustomerController::class, 'show'])->name('customers.show');
 });
 
 require __DIR__.'/settings.php';
