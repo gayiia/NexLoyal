ffef2b11eda3a6b9a08a2d70d858df213aebde57
gayiia <gayindus5@gmail.com>
2026-01-04 13:30:52 +0530

    setting blade and nav blade update

--- routes/settings.php
+++ routes/settings.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
+use App\Http\Controllers\Settings\TierRuleController;
 use App\Http\Controllers\Settings\TwoFactorAuthenticationController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::middleware('auth')->group(function () {
     Route::redirect('settings', '/settings/profile');
@@ -20,8 +20,14 @@ Route::middleware('auth')->group(function () {
         ->name('user-password.update');
 
     Route::get('settings/appearance', function () {
-        return Inertia::render('settings/appearance');
+        return view('settings.appearance');
     })->name('appearance.edit');
+
+    Route::get('settings/tier-rules', [TierRuleController::class, 'index'])->name('tier-rules');
+    Route::post('settings/tier-rules', [TierRuleController::class, 'store'])->name('tier-rules.store');
+    Route::patch('settings/tier-rules/{tier}', [TierRuleController::class, 'update'])->name('tier-rules.update');
+    Route::patch('settings/tier-rules/{tier}/status', [TierRuleController::class, 'updateStatus'])->name('tier-rules.status');
+    Route::delete('settings/tier-rules/{tier}', [TierRuleController::class, 'destroy'])->name('tier-rules.destroy');
 
     Route::get('settings/two-factor', [TwoFactorAuthenticationController::class, 'show'])
         ->name('two-factor.show');
