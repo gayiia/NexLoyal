687a53f12fc7bcc19733559612eea0174181543c
gayiia <gayindus5@gmail.com>
2026-01-04 09:41:42 +0530

    Web routes update

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return redirect()->route('login');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
+        return view('dashboard');
     })->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
+
+
